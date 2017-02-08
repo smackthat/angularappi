57ee0b766c8f5aeae0ec8298b700ebbd0a662370
Johannes Stenberg <johannes.stenberg@headpower.fi>
2017-02-08 13:52:51 +0200

    First commit

---
+++ TwerkFramework/ext-modules/psFramework/psFramework.component.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("psFramework")
+    .component("psFramework", {
+        transclude: false,
+        bindings: {
+
+        },
+        controller: "psFrameworkController",
+        templateUrl: "ext-modules/psFramework/psFramework.template.html"
+    });
+
+
+})();
---
+++ TwerkFramework/ext-modules/psFramework/psFramework.controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("psFramework")
+    .controller("psFrameworkController",
+    ['$scope',
+        function ($scope) {
+
+        }
+    ]);
+
+
+})();
